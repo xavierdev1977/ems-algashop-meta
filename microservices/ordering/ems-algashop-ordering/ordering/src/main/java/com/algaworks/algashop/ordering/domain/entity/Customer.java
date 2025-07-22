a3c206719b256666c16ec139b846af76daaa16d1
xavierdev1977 <xavier.rodrigo.fabiano@gmail.com>
2025-07-22 08:09:33 -0300

    Implementando a funcionalidade de pontos de lealdade

--- microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
+++ microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
@@ -2,7 +2,6 @@ package com.algaworks.algashop.ordering.domain.entity;
 
 import com.algaworks.algashop.ordering.domain.exception.CustomerArchivedException;
 import com.algaworks.algashop.ordering.domain.validator.FieldValidations;
-import org.apache.commons.validator.routines.EmailValidator;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
@@ -55,8 +54,12 @@ public class Customer {
         this.setLoyaltyPoints(loyaltyPoints);
     }
 
-    public void addLoayltyPoints(Integer points) {
-
+    public void addLoyaltyPoints(Integer loaylPointsAdded) {
+        verifyIfChangeable();
+        if (loaylPointsAdded <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.setLoyaltyPoints(this.loyaltyPoints() + loaylPointsAdded);
     }
 
     public void archive() {
@@ -200,6 +203,9 @@ public class Customer {
 
     private void setLoyaltyPoints(Integer loyaltyPoints) {
         Objects.requireNonNull(loyaltyPoints);
+        if(loyaltyPoints < 0){
+            throw new IllegalArgumentException();
+        }
         this.loyaltyPoints = loyaltyPoints;
     }
 
--- microservices/ordering/ems-algashop-ordering/ordering/src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
+++ microservices/ordering/ems-algashop-ordering/ordering/src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
@@ -107,4 +107,44 @@ class CustomerTest {
                 .isThrownBy(()-> customer.disablePromotionNotifications());
     }
 
+    @Test
+    void given_brandNewCustomer_whenAddLoyaltyPoints_shouldSumPoints() {
+        Customer customer = new Customer(
+                IdGenerator.generateTimeBasedUUID(),
+                "John Doe",
+                LocalDate.of(1991, 7, 5),
+                "john.doe@gmail.com",
+                "478-256-2504",
+                "255-08-0578",
+                false,
+                OffsetDateTime.now()
+        );
+
+        customer.addLoyaltyPoints(10);
+        customer.addLoyaltyPoints(20);
+
+        Assertions.assertThat(customer.loyaltyPoints()).isEqualTo(30);
+
+    }
+
+    @Test
+    void given_brandNewCustomer_whenAddInvalidLoyaltyPoints_shouldGenerateException() {
+        Customer customer = new Customer(
+                IdGenerator.generateTimeBasedUUID(),
+                "John Doe",
+                LocalDate.of(1991, 7, 5),
+                "john.doe@gmail.com",
+                "478-256-2504",
+                "255-08-0578",
+                false,
+                OffsetDateTime.now()
+        );
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(()-> customer.addLoyaltyPoints(0));
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(()-> customer.addLoyaltyPoints(-10));
+    }
+
 }
