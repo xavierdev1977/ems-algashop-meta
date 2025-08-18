e5d9b79dfdf99693a92b95457eef0968155cd7b0
xavierdev1977 <xavier.rodrigo.fabiano@gmail.com>
2025-08-18 08:58:49 -0300

    6.19. Controlando alteração de status

--- microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/domain/entity/OrderStatus.java
+++ microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/domain/entity/OrderStatus.java
@@ -1,9 +1,27 @@
 package com.algaworks.algashop.ordering.domain.entity;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum OrderStatus {
     DRAFT,
-    PLACED,
-    PAID,
-    READY,
-    CANCELED
+    PLACED(DRAFT),
+    PAID(PLACED),
+    READY(PAID),
+    CANCELED(PAID, READY, PLACED, DRAFT);
+
+    OrderStatus(OrderStatus... previousStatuses) {
+        this.previousStatuses = Arrays.asList(previousStatuses);
+    }
+
+    private final List<OrderStatus> previousStatuses;
+
+    public boolean canChangeTo(OrderStatus newStatus) {
+        OrderStatus currentStatus = this;
+        return newStatus.previousStatuses.contains(currentStatus);
+    }
+
+    public boolean canNotChangeTo(OrderStatus newStatus) {
+        return !canChangeTo(newStatus);
+    }
 }
