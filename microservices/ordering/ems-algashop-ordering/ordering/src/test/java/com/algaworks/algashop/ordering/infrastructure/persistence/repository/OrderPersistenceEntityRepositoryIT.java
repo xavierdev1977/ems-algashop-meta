e221528a0d04320db4b2836cc24970dc212cd939
xavierdev1977 <xavier.rodrigo.fabiano@gmail.com>
2026-02-03 09:53:17 -0300

    Isolando execucao dos testes de integracao

---
+++ microservices/ordering/ems-algashop-ordering/ordering/src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -0,0 +1,14 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPersistenceEntityRepositoryIT {
+
+    @Test
+    public void shouldRun() {
+
+    }
+
+}
