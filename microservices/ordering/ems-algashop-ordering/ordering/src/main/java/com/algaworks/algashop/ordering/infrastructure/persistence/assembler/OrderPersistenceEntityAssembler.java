cc06d764eb336b1a7dffb705628c13eefd1e4db5
xavierdev1977 <xavier.rodrigo.fabiano@gmail.com>
2026-02-24 09:57:54 -0300

    Copiando dados dos Value Objects para Embeddables e Reconstituindo Value Objects compostos

--- microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
+++ microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
@@ -1,6 +1,14 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.assembler;
 
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Address;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Billing;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Recipient;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Shipping;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.RecipientEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
 
@@ -23,7 +31,59 @@ public class OrderPersistenceEntityAssembler {
         orderPersistenceEntity.setCanceledAt(order.canceledAt());
         orderPersistenceEntity.setReadyAt(order.readyAt());
         orderPersistenceEntity.setVersion(order.version());
+        orderPersistenceEntity.setBilling(toBillingEmbeddable(order.billing()));
+        orderPersistenceEntity.setShipping(toShippingEmbeddable(order.shipping()));
         return orderPersistenceEntity;
+    }
+
+    private BillingEmbeddable toBillingEmbeddable(Billing billing) {
+        if (billing == null) {
+            return null;
+        }
+        return BillingEmbeddable.builder()
+                .firstName(billing.fullName().firstName())
+                .lastName(billing.fullName().lastName())
+                .document(billing.document().value())
+                .phone(billing.phone().value())
+                .address(toAddressEmbeddable(billing.address()))
+                .build();
+    }
+
+    private AddressEmbeddable toAddressEmbeddable(Address address) {
+        if (address == null) {
+            return null;
+        }
+        return AddressEmbeddable.builder()
+                .city(address.city())
+                .state(address.state())
+                .number(address.number())
+                .street(address.street())
+                .complement(address.complement())
+                .neighborhood(address.neighborhood())
+                .zipCode(address.zipCode().value())
+                .build();
+    }
+
+    private ShippingEmbeddable toShippingEmbeddable(Shipping shipping) {
+        if (shipping == null) {
+            return null;
+        }
+        var builder = ShippingEmbeddable.builder()
+                .expectedDate(shipping.expectedDate())
+                .cost(shipping.cost().value())
+                .address(toAddressEmbeddable(shipping.address()));
+        Recipient recipient = shipping.recipient();
+        if (recipient != null) {
+            builder.recipient(
+                    RecipientEmbeddable.builder()
+                            .firstName(recipient.fullName().firstName())
+                            .lastName(recipient.fullName().lastName())
+                            .document(recipient.document().value())
+                            .phone(recipient.phone().value())
+                            .build()
+            );
+        }
+        return builder.build();
     }
 
 }
--- microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
+++ microservices/ordering/ems-algashop-ordering/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
@@ -3,10 +3,13 @@ package com.algaworks.algashop.ordering.infrastructure.persistence.disassembler;
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.entity.OrderStatus;
 import com.algaworks.algashop.ordering.domain.model.entity.PaymentMethod;
-import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
-import com.algaworks.algashop.ordering.domain.model.valueobject.Quantity;
+import com.algaworks.algashop.ordering.domain.model.valueobject.*;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.RecipientEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
 
@@ -29,6 +32,43 @@ public class OrderPersistenceEntityDisassembler {
                 .readyAt(persistenceEntity.getReadyAt())
                 .items(new HashSet<>())
                 .version(persistenceEntity.getVersion())
+                .build();
+    }
+
+    private Shipping toShippingValueObject(ShippingEmbeddable shippingEmbeddable) {
+        RecipientEmbeddable recipientEmbeddable = shippingEmbeddable.getRecipient();
+        return Shipping.builder()
+                .cost(new Money(shippingEmbeddable.getCost()))
+                .expectedDate(shippingEmbeddable.getExpectedDate())
+                .recipient(
+                        Recipient.builder()
+                                .fullName(new FullName(recipientEmbeddable.getFirstName(), recipientEmbeddable.getLastName()))
+                                .document(new Document(recipientEmbeddable.getDocument()))
+                                .phone(new Phone(recipientEmbeddable.getPhone()))
+                                .build()
+                )
+                .address(toAddressValueObject(shippingEmbeddable.getAddress()))
+                .build();
+    }
+
+    private Billing toBillingValueObject(BillingEmbeddable billingEmbeddable) {
+        return Billing.builder()
+                .fullName(new FullName(billingEmbeddable.getFirstName(), billingEmbeddable.getLastName()))
+                .document(new Document(billingEmbeddable.getDocument()))
+                .phone(new Phone(billingEmbeddable.getPhone()))
+                .address(toAddressValueObject(billingEmbeddable.getAddress()))
+                .build();
+    }
+
+    private Address toAddressValueObject(AddressEmbeddable address) {
+        return Address.builder()
+                .street(address.getStreet())
+                .number(address.getNumber())
+                .complement(address.getComplement())
+                .neighborhood(address.getNeighborhood())
+                .city(address.getCity())
+                .state(address.getState())
+                .zipCode(new ZipCode(address.getZipCode()))
                 .build();
     }
 
